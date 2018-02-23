3a5a4041602ba01d986cbc883dfa531b460cfb6e
Alexander Chu <alchu@mymail.mines.edu>
2018-02-22 18:58:02 -0700

    Finished Lab06A

--- app/src/main/java/com/csci448/alchu/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/csci448/alchu/criminalintent/CrimeActivity.java
@@ -18,8 +18,7 @@ public class CrimeActivity extends SingleFragmentActivity {
     }
     @Override
     protected Fragment createFragment() {
-        UUID crimeId = (UUID) getIntent()
-                .getSerializableExtra(EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         return CrimeFragment.newInstance(crimeId);
     }
 
